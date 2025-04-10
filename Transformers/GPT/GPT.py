f6a1971b04d65e8c304438b444fffd00400285e4
EEEEEKKO <2512546490@qq.com>
2025-04-10 22:50:16 +0800

    Update GPT

--- Transformers/GPT/GPT.py
+++ Transformers/GPT/GPT.py
@@ -1,7 +1,68 @@
 import torch 
 import torch.nn as nn
-from labml_nn.optimizers.configs import OptimizerConfigs
 from Classic_Transformers.Transformer import Encoder, Decoder
+
+from labml.configs import BaseConfigs, option, meta_config
+from labml_nn.optimizers import WeightDecay
+from typing import Tuple
+
+
+class OptimizerConfigs(BaseConfigs):
+    """
+    <a id="OptimizerConfigs"></a>
+
+    ## Optimizer Configurations
+    """
+
+    # Optimizer
+    optimizer: torch.optim.Adam
+
+    # Weight decay
+    weight_decay_obj: WeightDecay
+    # Whether weight decay is decoupled;
+    # i.e. weight decay is not added to gradients
+    weight_decouple: bool = True
+    # Weight decay
+    weight_decay: float = 0.0
+    # Whether weight decay is absolute or should be multiplied by learning rate
+    weight_decay_absolute: bool = False
+
+    # Whether the adam update is optimized (different epsilon)
+    optimized_adam_update: bool = True
+
+    # Parameters to be optimized
+    parameters: any
+
+    # Learning rate $\alpha$
+    learning_rate: float = 0.01
+    # Beta values $(\beta_1, \beta_2)$ for Adam
+    betas: Tuple[float, float] = (0.9, 0.999)
+    # Epsilon $\epsilon$ for adam
+    eps: float = 1e-08
+
+    # Momentum for SGD
+    momentum: float = 0.5
+    # Whether to use AMSGrad
+    amsgrad: bool = False
+
+    # Number of warmup optimizer steps
+    warmup: int = 2_000
+    # Total number of optimizer steps (for cosine decay)
+    total_steps: int = int(1e10)
+
+    # Whether to degenerate to SGD in AdaBelief
+    degenerate_to_sgd: bool = True
+
+    # Whether to use Rectified Adam in AdaBelief
+    rectify: bool = True
+
+    # Model embedding size for Noam optimizer
+    d_model: int
+
+    rho: float
+
+    def __init__(self):
+        super().__init__(_primary='optimizer')
 
 
 def subsequent_mask(seq_len):
@@ -29,4 +90,4 @@ class GPT(nn.Module):
 
         return x
 
-
+    
